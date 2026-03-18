1568645ae2b663a366395dd6524dc75a6f4c4273
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-17 19:28:09 -0600

    avance

--- inventario de producto/Program.cs
+++ inventario de producto/Program.cs
@@ -1,6 +1,23 @@
-﻿using System; using System.Collections.Generic; 
+﻿using System; 
+using System.Collections.Generic;
+using System.Threading.Channels;
 
 class Producto
 {
-
+    public string Nombre;
+    public double Precio;
+    public int Cantidad;
+    public double CalcularValorInventario() { return Precio * Cantidad; }
+    public string EvaluarStock()
+    {
+        if (Cantidad == 0) return "Sin existencia";
+        else if (Cantidad <= 5) return "Stock bajo";
+        else return "Stock suficiente";
+    }
+    public void MostrarDatos()
+    {
+        Console.WriteLine($"Producto: {Nombre} | Precio: Q{Precio:F2} | Cantidad: {Cantidad} | Total: Q{CalcularValorInventario():F2} | Estado: 
+    { EvaluarStock()}
+        ");
+    }
 }
