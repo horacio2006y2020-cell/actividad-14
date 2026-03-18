bad6413a8e5d960fa08fc875377a7e3ef457b9da
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-03-17 22:16:58 -0600

    inventario

--- inventario de productos/Program.cs
+++ inventario de productos/Program.cs
@@ -1,8 +1,8 @@
-﻿using System; 
+﻿using System;
 using System.Collections.Generic;
 using System.Threading.Channels;
 
-List <Producto> productos = new List<Producto>();
+List<Producto> productos = new List<Producto>();
 Console.WriteLine("¿cúantos productos desea registrar?");
 int n = int.Parse(Console.ReadLine());
 for (int i = 0; i < n; i++)
